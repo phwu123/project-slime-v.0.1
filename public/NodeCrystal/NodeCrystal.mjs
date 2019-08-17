9af3ba65a15606e4a4029d4e1d449919d1523747
Peter Hwu <peterbhwu@gmail.com>
2019-08-17 04:32:08 -0700

    fixed dimensions

--- public/NodeCrystal/NodeCrystal.mjs
+++ public/NodeCrystal/NodeCrystal.mjs
@@ -1,21 +1,26 @@
 const nodeCrystalTemplate = `
   <link rel="stylesheet" type="text/css" href="./NodeCrystal/NodeCrystal.css" />
-  <section class="scene">
+  <section class="scene dev-tilt">
     <article class="crystal dev-rotate">
-      <span class="crystal__face crystal__face-top-front"></span>
-      <span class="crystal__face crystal__face-top-right"></span>
-      <span class="crystal__face crystal__face-top-back"></span>
-      <span class="crystal__face crystal__face-top-left"></span>
-      <span class="crystal__face crystal__face-bottom-front"></span>
-      <span class="crystal__face crystal__face-bottom-right"></span>
-      <span class="crystal__face crystal__face-bottom-back"></span>
-      <span class="crystal__face crystal__face-bottom-left"></span>
+      <div class="crystal__hemisphere crystal__top">
+        <span class="crystal__face crystal__face-top-front"></span>
+        <span class="crystal__face crystal__face-top-right"></span>
+        <span class="crystal__face crystal__face-top-back"></span>
+        <span class="crystal__face crystal__face-top-left"></span>
+      </div>
+      <div class="crystal__hemisphere crystal__bottom">
+        <span class="crystal__face crystal__face-bottom-front"></span>
+        <span class="crystal__face crystal__face-bottom-right"></span>
+        <span class="crystal__face crystal__face-bottom-back"></span>
+        <span class="crystal__face crystal__face-bottom-left"></span>
+      </div>
     </article>
   </section>
 `
 
 // 5-12-13 triangle
 // 67.38, 22.62, 90
+// 5 * 2^(1/2) - 12
 
 customElements.define('node-crystal', 
   class nodeCrystal extends HTMLElement {
