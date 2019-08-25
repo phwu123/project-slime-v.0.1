29e2604b79608f5befcabf7499634745ee9a518e
Peter Hwu <peterbhwu@gmail.com>
2019-08-25 04:11:35 -0700

    v1 ring

--- public/NodeRing/NodeRing.mjs
+++ public/NodeRing/NodeRing.mjs
@@ -1,9 +1,14 @@
 const nodeRingTemplate = `
   <link rel="stylesheet" type="text/css" href="./NodeRing/NodeRing.css" />
   <section class="scene">
-    <div class="ring-container dev-ring-tilt">
-      <span class="class-ring"></span>
-    </div>
+    <article class="ring-container dev-ring-tilt">
+      <div class="class-ring">
+        <node-crystal class="node-slot node-1"></node-crystal>
+        <node-crystal class="node-slot node-2"></node-crystal>
+        <node-crystal class="node-slot node-3"></node-crystal>
+        <node-crystal class="node-slot node-4"></node-crystal>
+      </div>
+    </article>
   </section>
 `
 customElements.define('node-ring', 
